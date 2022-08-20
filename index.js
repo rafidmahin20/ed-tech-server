5e3db98e06f14cb2d57f9c28200b081fdd1ea64b
rafidmahin20 <submanmahin@gmail.com>
2022-08-20 16:40:49 +0600

    edited

--- index.js
+++ index.js
@@ -1,5 +1,31 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dkv8trr.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        
+    }
+    finally{}
+}
+
+run().catch(console.dir);
+
+app.get('/', (req, res) =>{
+    res.send('hello from apple')
+})
+
+app.listen(port,() =>{
+    console.log(`apple listening on port ${port}`)
+})
